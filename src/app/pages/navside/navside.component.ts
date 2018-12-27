33e8291dc88efc79b2eb2dc07286cce3f3dcaa1d
Edycar Reyes <edycarreyes@gmail.com>
2018-12-26 21:46:00 -0600

    configuracion del navside
    se configura el navside para que se muestre solo en el resto de paginas que no sea la principal, la de registrar, la de la plataforma o la de iniciar sesion

--- src/app/pages/navside/navside.component.ts
+++ src/app/pages/navside/navside.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navside',
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navside.component.scss']
 })
 export class NavsideComponent implements OnInit {
-
-  constructor() { }
+  mostrarNav: boolean;
+  constructor(
+    public router: Router
+  ) {
+    console.log(this.router.url);
+    if ((this.router.url === '/') || (this.router.url === '/ingresar') || this.router.url === '/plataforma') {
+      this.mostrarNav = false;
+    } else {
+      this.mostrarNav = true;
+    }
+  }
 
   ngOnInit() {
   }
