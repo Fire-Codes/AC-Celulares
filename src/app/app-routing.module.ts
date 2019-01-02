6ba7adf2b1be44b8e5ea7278f87792a021750c60
Edycar Reyes <edycarreyes@gmail.com>
2019-01-01 23:50:10 -0600

    se crea la ruta para el componente de inventario en el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { SeleccionarPlataformaComponent } from './pages/seleccionar-plataforma/seleccionar-plataforma.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'ingresar', component: LoginComponent},
   {path: 'registrar', component: SignupComponent},
   {path: 'plataforma', component: SeleccionarPlataformaComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'inventario', component: InventarioComponent}
 ];
 
 @NgModule({
