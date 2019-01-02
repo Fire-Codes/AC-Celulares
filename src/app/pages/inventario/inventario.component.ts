04f1d1bd8c8c98b3fdda7b79ddd1124cc720770c
Edycar Reyes <edycarreyes@gmail.com>
2019-01-02 01:04:33 -0600

    se diseña la tabla de productos en el componente inventario
    se diseña casi por completo la tabla de productos en el componente de inventarios. Hace falta agregarle aun mas atributos al diseño para finalizarla

--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+// importacion del componente navside
+import { NavsideComponent } from '../navside/navside.component';
+
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+
+
+export interface UserData {
+  id: string;
+  Nombre: string;
+  Marca: string;
+  Tipo: string;
+  Modelo: string;
+  Existencia: number;
+  pCompra: number;
+  pVenta: number;
+}
+/** Constants used to fill up our data base. */
+const TIPO: string[] = ['Accesorio', 'Repuesto', 'Celular', 'Herramienta', 'Otro'];
+const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+const MARCA: string[] = ['SAMSUNG', 'LG', 'HUAWEI', 'SONY', 'APPLE'];
+const MODELO: string[] = ['GALAXY S7', 'G5', 'P20 Pro', 'XPERIA Z4', 'iPhone 5S'];
 
 @Component({
   selector: 'app-inventario',
@@ -7,9 +31,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventarioComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  ngOnInit() {
+  displayedColumns: string[] = ['id', 'Nombre', 'Marca', 'Modelo', 'Tipo', 'Existencia', 'pCompra', 'pVenta'];
+  dataSource: MatTableDataSource<UserData>;
+
+  constructor(
+    public nav: NavsideComponent
+  ) {
+    // se muestra el navside
+    this.nav.mostrarNav = true;
+
+    // Create 100 users
+    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    id: id.toString(),
+    Nombre: name,
+    Marca: MARCA[Math.round(Math.random() * (MARCA.length - 1))],
+    Tipo: TIPO[Math.round(Math.random() * (TIPO.length - 1))],
+    Modelo: MODELO[Math.round(Math.random() * (MODELO.length - 1))],
+    Existencia: Math.round(Math.random() * 100),
+    pCompra: Math.round(Math.random() * 1000),
+    pVenta: Math.round(Math.random() * 10000),
+  };
 }
