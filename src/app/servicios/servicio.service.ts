74eda58bfb36b3496e9e92de01e74ddb48fb5d63
Edycar Reyes <edycarreyes@gmail.com>
2019-01-30 17:58:55 -0600

    se crea la funcion aun sencilla para registrar nuevos usuarios. No se ha implementado su integracion con firestore.

--- src/app/servicios/servicio.service.ts
+++ src/app/servicios/servicio.service.ts
@@ -1,16 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
+// importaciones para el correcto funcionamiento del codigo
+import { promise } from 'protractor';
+import { reject } from 'q';
+import { map } from 'rxjs/operators';
+
+// importaciones de los componente de angularfire
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireStorage } from 'angularfire2/storage';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioService {
 
   constructor(
-    public router: Router
+    public router: Router,
+    public auth: AngularFireAuth
   ) { }
 
   public navegar(ruta: string) {
     this.router.navigate([`/${ruta}`]);
+  }
+
+  // funcion para registrar un nuevo usuario
+  public crearUsuario(email: string, contraseña: string) {
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      this.auth.auth.createUserWithEmailAndPassword(email, contraseña)
+        .then(datosUsuario => resolve(datosUsuario), err => reject(err))
+        .catch((err) => {
+          console.error(err);
+        });
+    });
   }
 }
