133d495547fdb7fb5d545b1a6412afb409d7a454
Edycar Reyes <edycarreyes@gmail.com>
2019-02-13 00:18:31 -0600

    se configura el modelo de datos para el componente de modales de agregar cliente

--- src/app/pages/modales/agregar-cliente/agregar-cliente.component.ts
+++ src/app/pages/modales/agregar-cliente/agregar-cliente.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarClienteComponent implements OnInit {
 
+  // variables para los ngModel de cada input
+  primerNombre = '';
+  segundoNombre = '';
+  primerApellido = '';
+  segundoApellido = '';
+  tipoUsuario = '';
+  celular: number;
+  cedula = '';
+  sexo = '';
+  email = '';
+  departamento = '';
+  municipio = '';
+  direccion = '';
+  id = '';
+  nombreCompleto = '';
+  nombres = '';
+  apellidos = '';
+
+
+  // variables para emision de eventos
+  @Output() cerrarModalEvent = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  // funcion para emitir el evento para cerrar el modal
+  cerrarModal() {
+    this.cerrarModalEvent.emit();
+  }
+
+  // funcion para hacer mayusculas el texto ingresado
+  toUpper() {
+    this.primerNombre = this.primerNombre.toUpperCase();
+    this.segundoNombre = this.segundoNombre.toUpperCase();
+    this.primerApellido = this.primerApellido.toUpperCase();
+    this.segundoApellido = this.segundoApellido.toUpperCase();
+    this.cedula = this.cedula.toUpperCase();
+  }
 }
