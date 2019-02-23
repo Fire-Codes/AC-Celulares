2f8fcf88fc3199943c444829ddc2e9ec75f293af
Edycar Reyes <edycarreyes@gmail.com>
2019-02-23 16:30:27 -0600

    se agrega el modal para reservar los productos

---
+++ src/app/pages/modales/reservar-producto/reservar-producto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservar-producto',
+  templateUrl: './reservar-producto.component.html',
+  styleUrls: ['./reservar-producto.component.scss']
+})
+export class ReservarProductoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
