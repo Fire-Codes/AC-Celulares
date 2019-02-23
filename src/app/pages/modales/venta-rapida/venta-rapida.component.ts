5fcc5c78ae08c7b3c177196cc39e2d31cb9ef74b
Edycar Reyes <edycarreyes@gmail.com>
2019-02-23 01:12:48 -0600

    se configura la plantilla para el modal de venta rapida

--- src/app/pages/modales/venta-rapida/venta-rapida.component.ts
+++ src/app/pages/modales/venta-rapida/venta-rapida.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+// se importan las interfaces
+import { Usuario } from './../../../interfaces/usuario';
+import { Producto } from './../../../interfaces/producto';
+import { ProductoFactura } from './../../../interfaces/producto-factura';
+
+// importacion del servicio
+import { ServicioService } from 'src/app/servicios/servicio.service';
+
+// importacion de los componentes de la base de datos
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireDatabase } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-venta-rapida',
@@ -7,9 +20,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VentaRapidaComponent implements OnInit {
 
-  constructor() { }
+  // variable que emitira el evento para cuando se deba de cerra el modal
+  @Output() cerrarModalVentaRapida = new EventEmitter();
+
+  // variables que contendran ya se al usuario o al producto inicial
+  @Input() usuario: Usuario = null;
+  @Input() producto: Producto = null;
+
+  constructor(
+    public servicio: ServicioService,
+    public fs: AngularFirestore,
+    public db: AngularFireDatabase
+  ) { }
 
   ngOnInit() {
   }
 
+  // funcion para cerra el modal
+  cerrarModal() {
+    this.cerrarModalVentaRapida.emit();
+  }
 }
