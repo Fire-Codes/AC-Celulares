0df7b558e62d43a45f72d5ae6125650ce8334e92
Edycar Reyes <edycarreyes@gmail.com>
2019-02-23 16:31:02 -0600

    se crea la tabla para el sistema de apartado

---
+++ src/app/pages/tablas/sistema-apartado/sistema-apartado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sistema-apartado',
+  templateUrl: './sistema-apartado.component.html',
+  styleUrls: ['./sistema-apartado.component.scss']
+})
+export class SistemaApartadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
